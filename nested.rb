3646f78837383932ee7a941af7f5fa663f571854
Joshua D <joshuadelapenha@yahoo.com>
2020-07-22 10:06:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,9 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:grace_hopper]
+  
 end
 
 def alan_kay_is_known_for
@@ -36,6 +38,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    
+  programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,7 +59,9 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:dennis_ritchie][:languages][0]
+  
 end
 
 def adding_matz
@@ -81,6 +87,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
+
+  programmer_hash[:yukihiro_matsumoto] = :{}
+  programmer_hash[:yukihiro_matsumoto][] = :known_for
+
 
 end
 
