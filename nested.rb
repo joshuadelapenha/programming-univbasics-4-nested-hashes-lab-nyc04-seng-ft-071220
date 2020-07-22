22f37706daadaab7cd7783cfe173247003d6f9ea
Joshua D <joshuadelapenha@yahoo.com>
2020-07-22 10:06:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,8 +88,8 @@ def adding_matz
       }
     }
 
-  programmer_hash[:yukihiro_matsumoto] = :{}
-  programmer_hash[:yukihiro_matsumoto][] = :known_for
+  programmer_hash[:yukihiro_matsumoto]
+  programmer_hash[:yukihiro_matsumoto][:known_for]
 
 
 end
