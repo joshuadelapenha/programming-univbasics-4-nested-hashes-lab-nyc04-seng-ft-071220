50d09e8361b8dbc4943fe32d864864d54f22da35
Joshua D <joshuadelapenha@yahoo.com>
2020-07-22 10:08:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -90,6 +90,7 @@ def adding_matz
 
   programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
   programmer_hash[:yukihiro_matsumoto][:languages][0] = "LISP"
+  programmer_hash[:yukihiro_matsumoto][:languages][0] = "C"
 
 
 end
